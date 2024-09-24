ebd296824a812fca0b8eed9a0cac4da631cc7352
Andalib Rahman Shagoto <andalibrahman369@gmail.com>
2024-09-24 19:23:41 +0600

    Banner section

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
     $(".navbar-mobile__details").click(function() {
         $(".navbar-mobile__details--address").toggleClass("navbar__details--active");
     });
+
+    $(".banner__d-type button").click(function() {
+        $(".banner__d-type button").removeClass("banner__d-type--active");
+        $(this).addClass("banner__d-type--active");
+    });
 });
