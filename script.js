87d48c99b47186be136ae5b54140a3e17c060317
Andalib Rahman Shagoto <andalibrahman369@gmail.com>
2024-09-24 15:25:39 +0600

    Navbar

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 $(document).ready(function() {
-    
+    $(".navbar-mobile__details").click(function() {
+        $(".navbar-mobile__details--address").toggleClass("navbar__details--active");
+    });
 });
