202118044506fc2a6190795ceff30c0f3ab13a0d
Andalib Rahman Shagoto <andalibrahman369@gmail.com>
2024-09-25 18:31:12 +0600

    Popular item

--- script.js
+++ script.js
@@ -7,4 +7,22 @@ $(document).ready(function() {
         $(".banner__d-type button").removeClass("banner__d-type--active");
         $(this).addClass("banner__d-type--active");
     });
+
+    let popularItemSlider = new Splide(".popular-items__slider", {
+        type       : 'loop',
+        perPage    : 5,
+        perMove    : 1,
+        pagination : false,
+        breakpoints: {
+            1090: {
+                perPage: 3
+            },
+            800: {
+                perPage: 2
+            },
+            540: {
+                perPage: 1
+            }
+        }
+    }).mount();
 });
